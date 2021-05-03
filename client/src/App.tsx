17f36ff54f1ec5d45c4bad4741a2382e8174d3aa
noisyboy25 <dj402and@gmail.com>
2021-05-03 14:37:04 +0300

    Add httpOnly cookie, register, login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react';
 import Form from './components/Form';
 
 function App() {
+  const [mode, setMode] = useState('register');
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = async () => {
+  const handleLogin = async () => {
     const loginData = { login, password };
     console.log(loginData);
 
@@ -18,15 +19,59 @@ function App() {
     console.log(data);
   };
 
+  const handleRegister = async () => {
+    const loginData = { login, password };
+    console.log(loginData);
+
+    const res = await fetch('/api/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(loginData),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+
   return (
     <div className="App">
       <header className="App-header"></header>
       <main>
-        <Form
-          setLogin={setLogin}
-          setPassword={setPassword}
-          handleSubmit={handleSubmit}
-        />
+        <div>
+          <div>
+            <input
+              id="register-mode"
+              type="radio"
+              value="register"
+              checked={mode === 'register'}
+              onChange={(event) => setMode(event.target.value)}
+            />
+            <label htmlFor="register-mode">Register</label>
+          </div>
+          <div>
+            <input
+              id="login-mode"
+              type="radio"
+              value="login"
+              checked={mode === 'login'}
+              onChange={(event) => setMode(event.target.value)}
+            />
+            <label htmlFor="login-mode">Login</label>
+          </div>
+        </div>
+
+        {mode === 'register' ? (
+          <Form
+            setLogin={setLogin}
+            setPassword={setPassword}
+            handleSubmit={handleRegister}
+          />
+        ) : (
+          <Form
+            setLogin={setLogin}
+            setPassword={setPassword}
+            handleSubmit={handleLogin}
+          />
+        )}
 
         <div>
           <div>Login: {login}</div>
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,45 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import path from 'path';
+import jwt from 'jsonwebtoken';
 
 const PORT = process.env.PORT || 5000;
+
+let users: { login: string; password: string }[] = [];
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get('/hello', (req, res) => {
   res.send('Hello World');
 });
 
-app.post('/api/login', (req, res) => {
+app.post('/api/register', (req, res) => {
+  const data = req.body;
   console.log(req.body);
+  users.push({ login: data.login, password: data.password });
+  res.send({ message: 'Successfully registered' });
+});
+
+app.post('/api/login', (req, res) => {
+  const data = req.body;
+  console.log(data);
+
+  const user: any = users.find((u) => u.login === data.login);
+
+  if (user.password !== data.password) {
+    res.status(401).send('Incorrect login or password');
+  }
+
+  const token = jwt.sign({ login: data.login }, 'secret');
+  console.log(token);
+
+  res.cookie('JWT', token, { httpOnly: true });
+  res.send({ message: 'Successfully logged in' });
 });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
